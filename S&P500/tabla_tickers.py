49bb74b7ab498074b743866f0d762d96ead67199
anivi <alejandro.nivon@culturacolectiva.com>
2018-06-13 16:53:33 -0500

    tab

--- S&P500/tabla_tickers.py
+++ S&P500/tabla_tickers.py
@@ -4,23 +4,35 @@ import requests
 import bs4
 import sys
 
-def get_page(url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+def get_page(url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'):
 
-	pag = requests.get(url)
+	try:
+		pag = requests.get(url)
+	except ValueError:
+		print("URL not a web page. Try again...")
+		return 0
 
-if page.status_code==200:
-	print('URL ok, se extrae la info')
-	text = pag.text
-else:
-	print(pag + 'URL ERROR, sin info')
-	text = 0;
+	
 
-return text
+	if pag.status_code == 200:
+		print('URL ok, se extrae la info')
+		text = pag.text
+	else: 
+		print(pag + 'URL ERROR, sin info')
+		text = 0
+
+	return text
 
 if __name__ == "__main__":
    
-   url = sys.argv
-   print(url)
+	url = sys.argv
+
+	if len(url) == 2:
+		text = get_page(url)
+	else:
+		text = get_page()
+
+	print(text)
 
    #text = gat_page(url)
 
