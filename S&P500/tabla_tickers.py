fd9a7f6033100a56bf21c99057cdf1f3e5818b74
anivi <alejandro.nivon@culturacolectiva.com>
2018-07-04 19:15:07 -0500

    Tabla completa

--- S&P500/tabla_tickers.py
+++ S&P500/tabla_tickers.py
@@ -1,39 +1,51 @@
+from datetime import datetime
+import re
 import pandas as pd
-import numpy as np
-import requests
 import bs4
-import sys
+import urllib
 
-def get_page(url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'):
+def get_SP_tab()
 
-	try:
-		pag = requests.get(url)
-	except ValueError:
-		print("URL not a web page. Try again...")
-		return 0
+	url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 
-	
+	page = urllib.request.urlopen(url).read()
+	soup = bs4.BeautifulSoup(page)   
+	tab = soup.select('table')[0].select('td')
 
-	if pag.status_code == 200:
-		print('URL ok, se extrae la info')
-		text = pag.text
-	else: 
-		print(pag + 'URL ERROR, sin info')
-		text = 0
+	texto = []
 
-	return text
+	for i in tab:
+		texto.append(i.text)
+	texto.insert(0, ' ')
 
-if __name__ == "__main__":
-   
-	url = sys.argv
+	tick = []
+	comp = []
+	hc = []
+	div = []
 
-	if len(url) == 2:
-		text = get_page(url)
-	else:
-		text = get_page()
+	for n in range(int(len(texto)/9)):
+		ind = 9*(n)
+		if(ind < len(texto)):
+			tick.append(texto[ind+1])
+			comp.append(texto[ind+2])
+			hc.append(texto[ind+6])
+			div.append(texto[ind+4])
 
-	print(text)
+	return pd.DataFrame({'Ticker':tick, 'Compañia':comp, 'HeadCuarters':hc, 'Division':div})
 
-   #text = gat_page(url)
+def get_timestamp(fecha):
+	tme = datetime.strptime('20.12.2016 09:38:42,76',
+                           '%d.%m.%Y %H:%M:%S,%f')
 
+
+def get_ticker_info(ticker, fecha_inicio, fecha_fin):
+	#Se obtiene información diaria de la acción
+
+	url = https://query1.finance.yahoo.com/v7/finance/download/ORCL?period1=1496552400&period2=1528088400&interval=1d&events=history&crumb=pJOrJOlKwNZ
+
+if __name__ == '__main__':
+	print(Obteniendo Tickers)
+	base = get_SP_tab()
+	tick = tab.Ticker.tolist()
+	timestamp2 = datetime.datetime.now().timestamp()
 
