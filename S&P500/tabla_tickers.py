10c50d1a4dd78c9d36c4f86075ffb4b621e63457
anivi <alejandro.nivon@culturacolectiva.com>
2018-07-14 11:54:56 -0500

    getting info on a dic

--- S&P500/tabla_tickers.py
+++ S&P500/tabla_tickers.py
@@ -1,15 +1,21 @@
 from datetime import datetime
-import re
+from datetime import timedelta
 import pandas as pd
+import numpy as np
+import re
+import json
+import requests
 import bs4
 import urllib
+import time
+import csv
 
 def get_SP_tab():
 
 	url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 
 	page = urllib.request.urlopen(url).read()
-	soup = bs4.BeautifulSoup(page)   
+	soup = bs4.BeautifulSoup(page, 'lxml')   
 	tab = soup.select('table')[0].select('td')
 
 	texto = []
@@ -33,18 +39,53 @@ def get_SP_tab():
 
 	return pd.DataFrame({'Ticker':tick, 'Compañia':comp, 'HeadCuarters':hc, 'Division':div})
 
-#def get_timestamp(fecha):
-#	tme = datetime.strptime('20.12.2016 09:38:42,76',
-#                           '%d.%m.%Y %H:%M:%S,%f')
+def get_timestamp(fecha, dias):
+#Se da timestamp1 = fecha, timestamp2 = fecha - dias
 
+	timestamp2 = int(
+					time.mktime(datetime.strptime(
+										fecha.strftime("%d/%m/%Y"), "%d/%m/%Y").\
+					timetuple())
+					)
+	timestamp1 = int(
+					time.mktime(datetime.strptime(
+										(fecha - timedelta(days = dias)).strftime("%d/%m/%Y"),
+										 "%d/%m/%Y").\
+					timetuple())
+					)
+	return timestamp1, timestamp2
 
-#def get_ticker_info(ticker, fecha_inicio, fecha_fin):
-	#Se obtiene información diaria de la acción
+def get_ticker_tab(tick, TS1, TS2):
+#obtiene el resultado de la lectura del ticker.
+#Intervalo: 1 día.
 
-#	url = https://query1.finance.yahoo.com/v7/finance/download/ORCL?period1=1496552400&period2=1528088400&interval=1d&events=history&crumb=pJOrJOlKwNZ
+	url = 'https://query1.finance.yahoo.com/v8/finance/chart/'+tick+'?symbol='+tick+'&period1='+str(TS1)+'&period2='+str(TS2)+\
+		'&interval=1d&includePrePost=true&events=div%2Csplit'
+
+	page = str(
+		urllib.request.urlopen(url).read()
+		)[1:]
+	page = re.sub(r"'", r"", page)
+
+	dic = json.loads(page)['chart']['result'][0]
+
+	return dic
+
+def structure_table(dic):
+
+	return df
 
 if __name__ == '__main__':
-	print('Obteniendo Tickers')
+	print('\n\t\t--- Obteniendo Tickers ---\n')
 	base = get_SP_tab()
 	tick = base.Ticker.tolist()
-#	timestamp2 = datetime.datetime.now().timestamp()
+	today = datetime.now()
+	TS1, TS2 = get_timestamp(today, 500)
+	tickers = base.Ticker.tolist()
+	
+	print('\n\t   ----- Obteniendo información -----\n')
+
+	info = get_ticker_tab(tickers[0], TS1, TS2)
+
+	print(info)
+
